64be99bbd0579023c319442b4280c48b807f53f4
Johna N. Morris <johna.n.morris@gmail.com>
2017-01-07 17:32:55 -0800

    added Ember serializers and modified Rails serializers to display phoneme info without an Ember phoneme model

--- app/serializers/language_serializer.rb
+++ app/serializers/language_serializer.rb
@@ -1,3 +1,4 @@
 class LanguageSerializer < ActiveModel::Serializer
   attributes :id, :name, :description
+  has_many :phonemes
 end
--- app/serializers/phoneme_serializer.rb
+++ app/serializers/phoneme_serializer.rb
@@ -1,11 +1,11 @@
 class PhonemeSerializer < ActiveModel::Serializer
 
-  # Modify serializable_hash to exclude nil/null values
-  def serializable_hash(adapter_options = nil, options = {}, adapter_instance = self.class.serialization_adapter_instance)
-    hash = super
-    hash.each { |key, value| hash.delete(key) if value.nil? }
-    hash
-  end
+  # # Modify serializable_hash to exclude nil/null values
+  # def serializable_hash(adapter_options = nil, options = {}, adapter_instance = self.class.serialization_adapter_instance)
+  #   hash = super
+  #   hash.each { |key, value| hash.delete(key) if value.nil? }
+  #   hash
+  # end
 
-  attributes :ipa, :voice, :consonant, :place, :manner, :high, :front, :low, :back, :syllabic
+  attributes :id, :ipa, :voice, :consonant, :place, :manner, :high, :front, :low, :back, :syllabic
 end
