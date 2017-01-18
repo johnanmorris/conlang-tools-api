6c8e85e6612926ff5a6aff90fa00ebf9b8ada1e5
Johna N. Morris <johna.n.morris@gmail.com>
2017-01-18 11:53:32 -0800

    editing phoneme model

--- app/serializers/language_serializer.rb
+++ app/serializers/language_serializer.rb
@@ -1,5 +1,6 @@
 class LanguageSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :phoneme_ids
-  has_many :phonemes
-  has_many :words
+  attributes :id, :name, :description
+
+  has_many :phonemes, embed: :ids, embed_in_root: true
+  has_many :words, embed: :ids, embed_in_root: true
 end
