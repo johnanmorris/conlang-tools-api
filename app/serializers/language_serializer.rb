2ba5ff1f54cfbc50a57b0b53726925f6b095d6b2
Johna N. Morris <johna.n.morris@gmail.com>
2017-01-19 13:02:50 -0800

    reverted changes to language model and phoneme model

--- app/serializers/language_serializer.rb
+++ app/serializers/language_serializer.rb
@@ -1,6 +1,5 @@
 class LanguageSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description
-
-  has_many :phonemes, embed: :ids, embed_in_root: true
+  attributes :id, :name, :description, :phoneme_ids
+  has_many :phonemes
   has_many :words, embed: :ids, embed_in_root: true
 end
--- app/serializers/phoneme_serializer.rb
+++ app/serializers/phoneme_serializer.rb
@@ -1,4 +1,3 @@
 class PhonemeSerializer < ActiveModel::Serializer
   attributes :id, :ipa, :voice, :place, :manner, :consonant, :syllabic, :high, :front, :low, :back
-  has_many :languages, embed: :ids, embed_in_root: true
 end
