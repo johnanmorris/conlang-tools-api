f6ea5f9a28d7b8f4514f6d96a3c5569e88015ab6
Johna N. Morris <johna.n.morris@gmail.com>
2017-01-20 09:51:18 -0800

    add new word to language

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,5 +1,5 @@
 class WordsController < ApplicationController
-  # before_action :find_language, except: [:destroy]
+  before_action :find_language, only: [:create, :update]
   before_action :find_word, only: [:show, :update, :destroy]
 
   def index
@@ -19,7 +19,6 @@ class WordsController < ApplicationController
     logger.info(">>>>>>>> #{request.body.read}")
     logger.info(">>>>>>>> #{params}")
     word = Word.new(word_params)
-    word.language_id = @language.id
 
     if word.save
       render json: word, status: :created
@@ -48,9 +47,9 @@ class WordsController < ApplicationController
     params.require(:word).permit(:id, :form, :translation, :language_id)
   end
 
-  # def find_language
-  #   @language = Language.find(params[:language_id])
-  # end
+  def find_language
+    @language = Language.find_by(id: params[:language_id])
+  end
 
   def find_word
     @word = Word.find(params[:id])
