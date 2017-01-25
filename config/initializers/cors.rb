5d0ef94edb1bdd921f87de5ea175cdbe51e055c4
Johna N. Morris <johna.n.morris@gmail.com>
2017-01-25 10:12:18 -0800

    modify cors to allow dev server

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@
 
 Rails.application.config.middleware.insert_before 0, "Rack::Cors" do
   allow do
-    origins 'http://conlang-engine.com'
+    origins 'http://conlang-engine.com', 'http://localhost:4200'
 
    resource '*',
      headers: :any,
